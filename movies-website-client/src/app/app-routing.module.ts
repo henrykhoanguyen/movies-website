5e6176b21647e034f15fefc9d0b61ff2b2af25d4
Khoa Nguyen <henrykhoanguyen@gmail.com>
2020-09-10 22:23:15 -0700

    created front-end using concept template

---
+++ movies-website-client/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+// Component
+import { MainPageComponent } from './pages/main-page/main-page.component';
+
+const routes: Routes = [
+  { path: '', component: MainPageComponent },
+  { path: 'index', redirectTo: '', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
